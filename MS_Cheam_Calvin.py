4d5d6aec4b1733d703a982a34a9472ea91876c7a
CCheam <ccheam25@gmail.com>
2026-02-13 11:44:56 -0600

    sq further setup

--- MS_Cheam_Calvin.py
+++ MS_Cheam_Calvin.py
@@ -4,10 +4,17 @@ import numpy
 import matplotlib.pyplot
 import os
 
+def ms_sq_generator():
+    print()
+
 def main():
 
     source =[]
-    goal = []
+    goal: [
+            [8 1 6], 
+            [3 5 7],
+            [4 9 2]
+           ] 
 
 if __name__ == "__main__":
     main()
