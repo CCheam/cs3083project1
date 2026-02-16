031e68d4b28d31a7c00f942921c5f2be96a08440
CCheam <ccheam25@gmail.com>
2026-02-16 11:17:21 -0600

    create data plotter, need to check functionality still

--- TSP_Cheam_Calvin.py
+++ TSP_Cheam_Calvin.py
@@ -1,13 +1,23 @@
 import heapq
 import time
-import numpy
-import matplotlib.pyplot
+import numpy as np
+import matplotlib.pyplot as plo
 import os
+
+def plotData(data):
+    #loads the dataset txt file name 
+    sel = np.loadtxt(data)
+    #plot calculations
+    x = data[:,0]
+    y = data[:,1]
+    plo.plot(x,y)
+    plo.show()
 
 def main():
 
     source =[]
     goal = []
+    plotData('TSP1.txt')
 
 if __name__ == "__main__":
     main()
