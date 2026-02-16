55c0b5c18fca6e807befa3fd8310545f976a307c
CCheam <ccheam25@gmail.com>
2026-02-16 11:46:08 -0600

    .

--- TSP_Cheam_Calvin.py
+++ TSP_Cheam_Calvin.py
@@ -4,15 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plo
 import os
 
+def txtToArr(file):
+    np.loadtxt(file)
+    return 
+
 def plotData(data):
     #loads the dataset txt file name 
-    sel = np.loadtxt(data)
+    #sel = np.loadtxt(data)
     #plot calculations
-    x = data[:,0]
-    y = data[:,1]
-    plo.plot(x,y)
+    #x = data[,0]
+    #y = data[,1]
+    plo.plot(x,y,marker='o')
     plo.show()
 
+def BF_TSP():
+
+    return ()
 def main():
 
     source =[]
