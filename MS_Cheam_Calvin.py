bc3e3476fc2269f20227fe1ce9e2bc921f27e04a
CCheam <ccheam25@gmail.com>
2026-02-17 22:12:20 -0600

    neighbor check function

--- MS_Cheam_Calvin.py
+++ MS_Cheam_Calvin.py
@@ -5,11 +5,34 @@ import matplotlib.pyplot
 import os
 
 def ms_sq_generator():
+    # create a 1-9 arr, shuffle order and set it as 3 by 3 arr like goal
     sq=np.array([1,2,3,4,5,6,7,8,9])
     np.random.shuffle(sq)
     ans=sq.reshape((3,3))
-    print(ans)
 
+def check_neighbors(current_state):
+    neighbors =[]
+    #flatten into single list
+    states=list(current_state)
+    #check all items
+    for i in range(3):
+        for j in range(3):
+            #ind is value under current item
+            ind = i*3+j
+            #check R swap
+            if j < 2:
+                new_states =states[:]
+                new_states[ind], new_states[ind+1] = new_states[ind+1], new_states[ind]
+                neighbors.append(tuple(new_states))
+            #check D swap
+            if i < 2:
+                new_states =states[:]
+                new_states[ind], new_states[ind+3] = new_states[ind+3], new_states[ind]
+                neighbors.append(tuple(new_states))
+    return neighbors
+
+def a_star_search():
+    return 
 def main():
 
     source =ms_sq_generator()
